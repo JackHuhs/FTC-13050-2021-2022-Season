8f7823db58529308c03db2ea3cc362636969563d
JackHuhs <72474445+JackHuhs@users.noreply.github.com>
2021-11-09 14:48:46 -0800

    Update Auton_Blue_SpinToWin.java

--- Auton_Blue_SpinToWin.java
+++ Auton_Blue_SpinToWin.java
@@ -13,6 +13,54 @@ import com.qualcomm.robotcore.util.Range;
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Blue_SpinToWin", group="Linear Opmode")
 //@Disabled
 public class Blue_SpinToWin extends LinearOpMode{
+    
+    //functions
+    public void driveForward(int dst, int spd){ //can also drive backwards with a negative speed
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        
+        leftDrive.setTargetPosition((int) (-dst));
+        rightDrive.setTargetPosition((int) (-dst));
+        
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        leftDrive.setVelocity((int) (spd));
+        rightDrive.setVelocity((int) (spd));
+        while (leftDrive.isBusy()||rightDrive.isBusy()){
+            telemetry.addData("position", leftDrive.getCurrentPosition());
+            telemetry.addData("position", rightDrive.getCurrentPosition());
+            telemetry.update();
+        }
+        leftDrive.setVelocity((int) (0));
+        rightDrive.setVelocity((int) (0));
+    }
+    
+    public void turn(int dgr, int spd){ //positive dgr is right, negative dgr is left
+        double turnPos = 575*dgr/90;
+        if (dgr < 0){
+            spd*=-1;
+        }
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        
+        leftDrive.setTargetPosition((int) (-turnPos));
+        rightDrive.setTargetPosition((int) (turnPos));
+        
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        leftDrive.setVelocity((int) (-spd));
+        rightDrive.setVelocity((int) (spd));
+        while (leftDrive.isBusy()||rightDrive.isBusy()){
+            telemetry.addData("position", leftDrive.getCurrentPosition());
+            telemetry.addData("position", rightDrive.getCurrentPosition());
+            telemetry.update();
+        }
+        leftDrive.setVelocity((int) (0));
+        rightDrive.setVelocity((int) (0));
+    }
+    
     private ElapsedTime runtime = new ElapsedTime();
     //variables
     DcMotorEx leftDrive;
@@ -34,51 +82,22 @@ public class Blue_SpinToWin extends LinearOpMode{
         //hardware map config
         leftDrive=hardwareMap.get(DcMotorEx.class,"left_drive");
         rightDrive=hardwareMap.get(DcMotorEx.class,"right_drive");
-        // leftDownDrive=hardwareMap.get(DcMotorEx.class,"left_down_drive");
-        // rightDownDrive=hardwareMap.get(DcMotorEx.class,"right_down_drive");
 
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        // leftDownDrive.setDirection(DcMotor.Direction.FORWARD);
-        // rightDownDrive.setDirection(DcMotor.Direction.REVERSE);
 
         //start
         waitForStart();
         runtime.reset();
 
         //actual autonomous path
-        //amount of inches they need to run to turn 90 degrees is 4.5π
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // leftDownDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // rightDownDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         
-        leftDrive.setTargetPosition((int) (575));
-        // leftDownDrive.setTargetPosition((int) (4.5/wheelDiameter)*TPR);
-        rightDrive.setTargetPosition((int) (-575));
-        // rightDownDrive.setTargetPosition((int) (-4.5/wheelDiameter)*TPR);
-
-        // leftDrive.setPower(power);
-        // rightDrive.setPower(-power);
-        // leftDownDrive.setPower(power);
-        // rightDownDrive.setPower(-power);
-
-        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        // leftDownDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        // rightDownDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        turn((int)(90),(int)(900));
         
-        leftDrive.setVelocity((int) (900));
-        rightDrive.setVelocity((int) (900));
-        while (leftDrive.isBusy()||rightDrive.isBusy()){
-            telemetry.addData("position", leftDrive.getCurrentPosition());
-            telemetry.addData("position", rightDrive.getCurrentPosition());
-            telemetry.update();
-        }
-        leftDrive.setVelocity((int) (0));
-        rightDrive.setVelocity((int) (0));
+        driveForward((int)(401),(int)(900));
         
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //SPIN DUCKS HERE
+        
+        turn((int)(-90),(int)(900));
     }
 }
